5014ef078c5493b9f64fbde7e3913c8ef8b2e38f
kyo <phongbinh2511@gmail.com>
2019-11-11 21:58:25 +0800

    Added comment to a function.

--- databaseutil/databaseutil.go
+++ databaseutil/databaseutil.go
@@ -172,6 +172,8 @@ func getUserFromDatabaseUsersTable(userName string, databasePtr *sql.DB) (User, 
 		errorMessage:   ""}
 }
 
+// UpdatePasswordAndResponseJsonOfUserFromDatabaseUsersTable updates the password of the user whose name is given in the context parameter and the requested JSON object.
+// Also, it responses to the client the json of the given user.
 func UpdatePasswordAndResponseJsonOfUserFromDatabaseUsersTable(databasePtr *sql.DB) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		userName := context.Param(userNameAttribute)
